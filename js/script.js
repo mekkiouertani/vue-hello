591b15130acd9891d50c7535007edc02b99e0285
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-27 15:42:11 +0200

    add button for change h1 color

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ const { createApp } = Vue
     createApp({
         data(){
             return{
-                message : "Hello Vue!"
+                message : "Hello Vue!",
+                colorBlue: "text-primary"
             }
         },
         methods: {
-
+            changeColor(){
+                this.colorBlue = this.colorBlue === "text-primary" ? "bg-warning" : "text-primary"
+            }
         }
     }).mount('#app-vue');
