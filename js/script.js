a48208c61f19c287af1560135c263ea2ca1aab96
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-28 16:02:43 +0200

    add condition if button clicked once

--- js/script.js
+++ js/script.js
@@ -9,19 +9,33 @@ const { createApp } = Vue
                 text: "Ciao", 
                 input: "",
                 getInput: "",
-                img: "https://hiringplatform.boolean.careers/images/logo.png"
+                img: "https://hiringplatform.boolean.careers/images/logo.png",
+                manyTimes: "volte",
+                
             }
         },
         methods: {
             btnEffect(){
+                
+                if(this.count === 0){
+                    this.manyTimes = "volta"
+                } else{
+                    this.manyTimes = "volte"
+                }
+
                 this.count++
                 this.colorBlue = this.colorBlue === "text-primary" ? "bg-warning" : "text-primary"
             },
-            clg(){
-                const inputEl = this.input
-                this.getInput = inputEl.charAt(0).toUpperCase() + inputEl.slice(1)
-                this.input = ""
+            printName(){
+                const inputValue = this.input
+                this.getInput = inputValue.charAt(0).toUpperCase() + inputValue.slice(1)
+                this.input = "";
                 console.log(this.getInput);
             },
+            printVolte(){
+                if(getInput === 1){
+                    manyTimes = "volta"
+                }
+            }
         }
     }).mount('#app-vue');
